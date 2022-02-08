e11522b6c3814b1d493f5b8ff66ba743373775a3
verissimo francis <verissimo francis>
2022-02-08 12:33:05 +0100

    Update Markup to use API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-import _ from 'lodash';
 import './style.css';
 
-const component = () => {
-  const element = document.createElement('div');
+const userName = document.querySelector('#username');
+const score = document.querySelector('#user-score');
+const scoreBoard = document.querySelector('#scores');
+const form = document.querySelector('.add-scores');
+const refreshBtn = document.querySelector('#btn');
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/E4fgdgzgDxgchhc/scores';
 
-  element.innerHTML = _.join(['', ''], ' ');
-
-  return element;
+const postData = () => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        user: userName.value,
+        score: score.value,
+      }),
+    });
+    form.reset();
+  });
 };
 
-document.body.appendChild(component());
+postData();
+
+const fetchData = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  scoreBoard.innerHTML = '';
+  data.result.forEach((e) => {
+    scoreBoard.innerHTML += `<p>${e.user}: ${e.score}</p>`;
+  });
+};
+
+refreshBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  fetchData();
+});
+
+window.addEventListener('load', (e) => {
+  e.preventDefault();
+  fetchData();
+});
